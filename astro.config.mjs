cb7e9239f26acac72d9dbe664cbfb144bf977692
lucraak <luctiemessen@gmail.com>
2023-12-23 17:35:45 +0100

    dep fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import markdoc from '@astrojs/markdoc';
 
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://luctiemessen.github.io',
-    integrations: [markdoc()],
 });
