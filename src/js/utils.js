250b3d90f1e1f76c75184a9d061a9530149a2719
lucraak <luctiemessen@gmail.com>
2024-01-06 20:00:13 +0100

    almost there

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,7 +1,7 @@
 export function formatDate(date) {
     return new Date(date).toLocaleDateString('en-US',{
         year: 'numeric',
-        month: 'long',
+        month: 'short',
         day: 'numeric',
       });
 }
