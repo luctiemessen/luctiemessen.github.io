c71ec3567d669dfc0324b6192401289bb10a1521
Luc Tiemessen <luctiemessen@gmail.com>
2024-01-22 21:15:22 +0100

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luctiemessen.github.io',
-  markdown: { shikiConfig: { theme: 'css-variables' } },
-  integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: 'css-variables'
+    }
+  },
+  integrations: [mdx(), sitemap()],
   // fix for malfunctioning sharp install.
-  image: { service: { entrypoint: 'astro/assets/services/noop'}},
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop'
+    }
+  }
 });
